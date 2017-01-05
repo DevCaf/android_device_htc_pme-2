cb47269fe5197c6648599c0774834e2128e6c229
wrapped <ddroidx@gmail.com>
2017-01-05 12:30:19 -0600

    pme: disable unused ril flag
    Change-Id: I11e429e8cbc3bd3e2943cb9e11f49b9fd3a0122b
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -197,7 +197,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/pme/releasetools
 
 # RIL
-TARGET_RIL_VARIANT := caf
+# TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
